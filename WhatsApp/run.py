c554850a63a6d783ea503cc0a7966ebe5d77a95f
Nikolai <ya.pikus@gmail.com>
2022-04-27 19:53:05 +0300

    Added requirements.txt

--- WhatsApp/run.py
+++ WhatsApp/run.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
         # Maintainer.update_table()
         Table = Maintainer.open_table()
         persons, phones = Maintainer.find_soon_day_birthday(Table)
-        print(phones)
+        del phones
         for person in persons:
             with open('WAB_logs.out', 'a', encoding='utf-8') as output:
                 print(WhatsAppBot.send_message(
-                    f'⚡ *Завтра* день рождения у:\n_{person}_ 🥳', ('7 977 700-88-13',)),
+                    f'⚡ *Завтра* день рождения у:\n_{person}_ 🥳', phones),
                     file=output
                 )
         time.sleep(86_400)  # 1 day :)
